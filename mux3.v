b921a614f0444eb072f4cc72e23ef3e3ba702afd
Zahra Moosavi <moosavizahra67@yahoo.com>
2019-04-06 17:41:38 +0430

    test git :D

--- mux3.v
+++ mux3.v
@@ -14,7 +14,6 @@ module mux3 (
     sel3;
     input [n-1:0]inp1, inp2, inp3;
     output [n-1:0]out;
-
     assign out = sel1 ? inp1 : 
                  sel2 ? inp2 : 
                  sel3 ? inp3 :
