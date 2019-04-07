f030545e52f482587c4e9c08a06547b5b449536b
ghminaei <ghminaei1037@gmail.com>
2019-04-07 19:52:42 +0430

    errors fixed

--- ALU.v
+++ ALU.v
@@ -10,6 +10,7 @@ module ALU (
     input [n-1:0]inp1, inp2;
     input [2:0]func;
     output [n-1:0]out;
+    output zero;
 
     wire [2:0] MOV = 3'b000, ADD = 3'b001, SUB = 3'b010, AND = 3'b011,
                OR  = 3'b100, NOT = 3'b101, NOP = 3'b110;
@@ -22,7 +23,7 @@ module ALU (
                  (func == NOT) ? ~inp2 :
                  (func == NOP) ? out :
                  out;
-    assign zero = (out == (n){1'b0}) ? 1'b1 :
+    assign zero = (out == {(n){1'b0}}) ? 1'b1 :
                   1'b0;
                  
 endmodule
