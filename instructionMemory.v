c469beb8c01804813d50a0001536b953a3034a43
unknown <moosavizahra67@yahoo.com>
2019-04-07 12:18:49 -0500

    instMemory has been updated

--- instructionMemory.v
+++ instructionMemory.v
@@ -7,8 +7,16 @@ module InstMemory (
     input [PCL-1:0]address;
     output [WORD-1:0]out;
     reg [WORD-1:0]memory[LENGTH-1:0];
-    initial begin 
-        $readmemb("instructions.txt", rom);
+   /* parameter NOP = 16'b1000000001000000;
+    initial begin
+        for (i = 0; i < LENGTH; i = i + 1) begin
+            memory[i] = NOP;
+        end
     end
+    */
+    initial begin 
+        $readmemb("instructions.txt", memory);
+    end
+
     assign out = memory[address];
 endmodule
