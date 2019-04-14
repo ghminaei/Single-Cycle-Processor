f98e620487a06e65e841166f5bc3d9bb54c5ac96
ghminaei <ghminaei1037@gmail.com>
2019-04-14 12:05:26 +0430

    Happy ending :))

--- regFile.v
+++ regFile.v
@@ -19,38 +19,36 @@
 	input writeEn, rst, clk;
 
 	output [15:0]readData1, readData2;
-
+	parameter W0 = 2'b00, W1 = 2'b01, W2 = 2'b10, W3 = 2'b11;
 	reg [2:0] reg1 = 3'b000;
 	reg [2:0] reg2 = 3'b000;
 	reg [2:0] regW = 3'b000;
 
 	reg [15:0] registers[7:0]; 
+	always @(window, readReg1, readReg2, writeReg) begin
 
-	always @(posedge rst,posedge clk ,window) begin
+		reg1 = (window == W0) ? {1'b0, readReg1} :
+						(window == W1) ? {1'b0, readReg1} + 3'b010 :
+						(window == W2) ? {1'b0, readReg1} + 3'b100 :
+						(window == W3) ? {1'b0, readReg1} + 3'b110 :
+						{1'b0, readReg1};
+						
+		reg2 = (window == W0) ? {1'b0, readReg2} :
+						(window == W1) ? {1'b0, readReg2} + 3'b010 :
+						(window == W2) ? {1'b0, readReg2} + 3'b100 :
+						(window == W3) ? {1'b0, readReg2} + 3'b110 :
+						{1'b0, readReg2};
+		
+		regW = (window == W0) ? {1'b0, writeReg} :
+						(window == W1) ? {1'b0, writeReg} + 3'b010 :
+						(window == W2) ? {1'b0, writeReg} + 3'b100 :
+						(window == W3) ? {1'b0, writeReg} + 3'b110 :
+						{1'b0, writeReg};
+	end
+					
+	always @(posedge rst,posedge clk) begin
 		if(rst) {registers[0],registers[1],registers[2],registers[3],registers[4],registers[5],registers[6],registers[7]} = 128'b0;
 		else begin
-			case(window)
-			2'b00: begin 
-				reg1 = {1'b0,readReg1};
-				reg2 = {1'b0,readReg2};
-				regW = {1'b0,writeReg};
-			   end
-			2'b01: begin 
-				reg1 = {1'b0,readReg1} + 3'b010;
-				reg2 = {1'b0,readReg2} + 3'b010;
-				regW = {1'b0,writeReg} + 3'b010;
-			   end
-			2'b10: begin 
-				reg1 = {1'b0,readReg1} + 3'b100;
-				reg2 = {1'b0,readReg2} + 3'b100;
-				regW = {1'b0,writeReg} + 3'b100;
-			   end
-			2'b11: begin 
-				reg1 = {1'b0,readReg1} + 3'b110;
-				reg2 = {1'b0,readReg2} + 3'b110;
-				regW = {1'b0,writeReg} + 3'b110;
-			   end
-			endcase
 			if(writeEn) registers[regW]= writeData;
 		end
 	end
